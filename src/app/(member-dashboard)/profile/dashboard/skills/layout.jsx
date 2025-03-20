7e9e810df4e58e4eb2d25241a4f50b1a3fb1789c
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2025-03-21 02:02:34 +0600

    fix: some issue

--- src/app/(member-dashboard)/profile/dashboard/skills/layout.jsx
+++ src/app/(member-dashboard)/profile/dashboard/skills/layout.jsx
@@ -5,7 +5,7 @@ import { COMING_SOON } from "@/lib/utils";
 export default function layout({ children }) {
     return (
         <>
-            {COMING_SOON ? <ComingSoon /> : children}
+            {COMING_SOON ? <ComingSoon title="Skills" /> : children}
         </>
     );
 }
